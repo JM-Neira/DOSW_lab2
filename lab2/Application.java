ac2a22313b80c08f02ae44db6ffdceae60b70fb8
Daniel Ahumada <danielahumada071@mail.com>
2026-02-06 12:08:31 -0500

    creando archivos iniciales

---
+++ lab2/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Proyecto en maven configurado correctamente");
+    }
+
+    Reto1TiendaDonPepe.ejecutar();
+}
---
+++ lab2/reto1/Reto1TiendaDonPepe.java
@@ -0,0 +1,5 @@
+public class Reto1TiendaDonPepe {
+    public static void ejecutar() {
+        System.out.println("Ejecutando Reto 1 - Tienda don Pepe");
+    }
+}
